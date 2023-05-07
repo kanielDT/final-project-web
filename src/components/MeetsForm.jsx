93179bdb456140c31e25a951688c11f83eca0204
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-05-07 11:43:04 -0400

    First commit:Created and cleaned out my react-app

---
+++ src/components/MeetsForm.jsx
@@ -0,0 +1,7 @@
+export default function MeetsForm() {
+    return (
+        <section>
+            Forms
+        </section>
+    )
+}
---
+++ src/components/MeetsPanel.jsx
@@ -0,0 +1,5 @@
+export default function MeetsPanel() {
+    return (
+        <section>Panel</section>
+    )
+}
